27e8679967805240524408218c908a0ab818ba08
Nadeesha Thilakaraathne <nadeesha9090@gmail.com>
2015-10-11 22:45:31 +0530

    OOP Project Folde

---
+++ PracticeOOP/src/practiceoop/StaticKeyWord/GraduateInfo.java
@@ -0,0 +1,29 @@
+package practiceoop.StaticKeyWord;
+
+//Java static keyword use in variables 
+public class GraduateInfo {
+    static int id = 0;
+    String name;
+    static String  universityName = "RUSL";
+    
+   
+    GraduateInfo(String n){
+        id++;
+        name = n;
+    }
+    
+    void displayGraduatesInfo(){
+        System.out.println("Id "+id);
+        System.out.println("Name "+name);
+        System.out.println("University Name "+ universityName+"\n\n");
+    }
+    
+    public static void main(String[] args) {
+        GraduateInfo student1 = new GraduateInfo("Amara");
+        student1.displayGraduatesInfo();  
+        GraduateInfo student2 = new GraduateInfo("Nayana");
+        student2.displayGraduatesInfo(); 
+        GraduateInfo student3 = new GraduateInfo("Kamal");
+        student3.displayGraduatesInfo(); 
+    }
+}
---
+++ PracticeOOP/src/practiceoop/StaticKeyWord/StaticKeywordTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practiceoop.StaticKeyWord;
+
+/**
+ *
+ * @author hp
+ */
+public class StaticKeywordTest {
+//Static variable 
+    // It make the memory use efficient 
+    //use to represent the common property of all objects
+    //Get memory in the class area only once when the class is loading 
+    
+    
+}
